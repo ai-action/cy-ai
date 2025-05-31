68a633c381b532119822bc003aa51ea1479cc303
Mark <mark@remarkablemark.org>
2025-05-31 19:25:53 -0400

    test(cypress): add e2e specs

---
+++ cypress/e2e/example.cypress.io.cy.ts
@@ -0,0 +1,9 @@
+describe('example.cypress.io', () => {
+  it('submits form', () => {
+    cy.ai('go to example.cypress.io and see "Kitchen Sink"');
+    cy.ai('click on link "submit"');
+    cy.ai("find label 'Coupon Code' and type 'HALFOFF'");
+    cy.ai("click button 'Submit'");
+    cy.ai("assert 'Your form has been submitted!'");
+  });
+});
