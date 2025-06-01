3677b5777473baa3e3b7382dca49f1aa337b4d85
Mark <mark@remarkablemark.org>
2025-06-01 12:31:31 -0400

    test(cypress): rename and refactor e2e specs

--- cypress/e2e/options.cy.ts
+++ cypress/e2e/options.cy.ts
@@ -1,5 +1,5 @@
-describe('example.com', () => {
-  it('sees heading', () => {
+describe('options', () => {
+  it('sets options', () => {
     cy.ai("open https://example.com and see heading 'Example Domain'", {
       log: false,
       timeout: 1000 * 60 * 5, // 5 minutes
