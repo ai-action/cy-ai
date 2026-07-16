5a7bdd77b131b89a44dd3b179b80cb592b28f29d
Mark <mark@remarkablemark.org>
2026-07-15 23:23:28 -0400

    build(package): upgrade TypeScript to 6.0.3

--- src/utils/options.ts
+++ src/utils/options.ts
@@ -1,8 +1,10 @@
 /* eslint-disable prefer-const */
+import type { Runnable } from '@langchain/core/runnables'
+
 import { chain } from './llm'
 import { minutes } from './time'
 
-export let llm = chain
+export let llm: Runnable = chain
 
 export let log = true
 
